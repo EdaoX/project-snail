0ef4f8596465e2a7454714c00226ba7d09d1a479
Matteo Giusti <suikofire@gmail.com>
2018-05-18 21:32:00 +0200

    Set up basic movement (mouse for now)

---
+++ Assets/ClickMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickMove : MonoBehaviour
+{
+
+	[SerializeField] Camera _camera;
+	[SerializeField] NavMeshAgent _agent;
+	
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit))
+			{
+				_agent.SetDestination(hit.point);
+			}
+			
+		}
+	}
+}
