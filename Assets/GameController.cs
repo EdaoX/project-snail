dc8061cfe567a080a89e69e4912d14a0337491b0
Matteo Giusti <suikofire@gmail.com>
2018-05-21 20:52:02 +0200

    Added simple hunger behavior and Camera follow

---
+++ Assets/GameController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class GameController : MonoBehaviour {
+
+	public const int TICK_PER_SECOND = 20;
+	private float _timeBetweenUpdates;
+	private float _elapsedTimeFromLastUpdate;
+
+	[SerializeField] private Snail _snail;
+	[SerializeField] private NavMeshSurface _navMesh;
+	
+	void Start ()
+	{
+		_timeBetweenUpdates = 1f / TICK_PER_SECOND;
+		_elapsedTimeFromLastUpdate = _timeBetweenUpdates;
+	}
+	
+	void Update ()
+	{
+		ComputeTick();
+		
+		if (!ShouldUpdate()) return;
+		
+		ExecuteLogic();
+		ResetTick();
+	}
+
+	void ExecuteLogic()
+	{
+		if (!_snail.HasTask() && _snail.IsHungry())
+		{
+			GameObject food = GameObject.FindWithTag("Food");
+			if (food != null)
+			{
+				_snail.SetDestination(food.transform.position);
+			}
+		}
+		
+		_snail.TickUpdate();
+	}
+
+	void ComputeTick()
+	{
+		_elapsedTimeFromLastUpdate = Mathf.Min(_elapsedTimeFromLastUpdate + Time.deltaTime, _timeBetweenUpdates);
+	}
+
+	bool ShouldUpdate()
+	{
+		return (_timeBetweenUpdates - _elapsedTimeFromLastUpdate) < Mathf.Epsilon;
+	}
+
+	void ResetTick()
+	{
+		_elapsedTimeFromLastUpdate = 0;
+	}
+}
---
+++ Assets/Snail.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Snail : MonoBehaviour
+{
+	protected const int HUNGER_THRESHOLD = 30;
+	
+	protected Vector3 currentDestination;
+	protected NavMeshAgent agent;
+	private bool _hasTask = false;
+	
+	// TODO - Remove from inspector
+	[SerializeField] [Range(0, 100)] private float _fullness = 100f;
+	[Tooltip("% per second")] [SerializeField] private float _hungerSpeed = 0.1f; // Hunger per second
+
+	// Pre-calculated for perfomance
+	private float _hungerFactor = 1f / GameController.TICK_PER_SECOND;
+
+	void Start()
+	{
+		currentDestination = transform.position;
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	public void SetDestination(Vector3 destination)
+	{
+		currentDestination = destination;
+	}
+
+	public void Update()
+	{
+	}
+
+	public void TickUpdate()
+	{
+		ProgressHunger();
+		
+		if (currentDestination != agent.destination)
+		{
+			agent.SetDestination(currentDestination);
+			_hasTask = true;
+		}
+
+		if (IsAtDestination())
+		{
+			_hasTask = false;
+		}
+	}
+
+	private void ProgressHunger()
+	{
+		_fullness = Mathf.Max(0, _fullness - _hungerSpeed * _hungerFactor);
+	}
+
+	public bool HasTask()
+	{
+		return _hasTask;
+	}
+
+	public bool IsHungry()
+	{
+		return _fullness <= HUNGER_THRESHOLD;
+	}
+
+	public bool IsAtDestination()
+	{
+		return agent.remainingDistance <= agent.stoppingDistance;
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		switch (other.gameObject.tag)
+		{
+			case "Food" :
+				// TODO - fullness info should belong to a Food class
+				_fullness = Mathf.Min(100, _fullness + 20);
+				Destroy(other.gameObject);
+				break;
+		}
+	}
+}
